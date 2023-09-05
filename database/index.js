73cb7b3a66c29438d66df2574da53be4c70656e5
koneksigithubiman <142722423+koneksigithubiman@users.noreply.github.com>
2023-09-05 10:26:53 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,19 +1,21 @@
-const connectToDb = require("../database")
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-const databaseMiddleware = async (req, res, next) => {
+const MONGO_PROD = `mongodb://mongo:MkIak5NxzVroNzP5bk0p@containers-us-west-203.railway.app:6164`
+const MONGO_URI= `mongodb://127.0.0.1:27017`
+
+const DB_URI = process.env.ENV === 'production' ? MONGO_PROD : MONGO_URI 
+const connectToDb = async () => {
   try {
-    const db = await connectToDb()
+    const mongoClient = await new MongoClient(DB_URI).connect();
+    db = mongoClient.db('assignment-11')
+
+    console.log(`You have been CONNECTED.. letss goo..!! `);
+    return db
+  } 
   
-    req.db = db
-  
-    next();
-}
+  catch (error) {
+    console.log(error);
+  }
+};
 
-catch (error) {
-  console.log(error, `<=================== error ==================`);
-}
-
-}
-
-module.exports = databaseMiddleware
+module.exports = connectToDb;
