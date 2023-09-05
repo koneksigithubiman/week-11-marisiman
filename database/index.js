51b69fa5321913a26e27c794c4b8986404a65977
koneksigithubiman <142722423+koneksigithubiman@users.noreply.github.com>
2023-09-05 10:25:17 +0700

    Create index.js

---
+++ database/index.js
@@ -0,0 +1,19 @@
+const connectToDb = require("../database")
+const { MongoClient } = require('mongodb')
+
+const databaseMiddleware = async (req, res, next) => {
+  try {
+    const db = await connectToDb()
+  
+    req.db = db
+  
+    next();
+}
+
+catch (error) {
+  console.log(error, `<=================== error ==================`);
+}
+
+}
+
+module.exports = databaseMiddleware
